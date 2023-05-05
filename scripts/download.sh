2cb39c61cc81d05cfe562acfbf0dbe663c5605bf
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-05-04 21:08:35 -0700

    Remove extra pip install

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,3 @@
-pip install gdown
 gdown --id 1b-ylF9DmfBvtTfhNJKeNq3Zs5W7yOHu5 --output shapenetcore_partanno_segmentation_benchmark_v0.zip
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
