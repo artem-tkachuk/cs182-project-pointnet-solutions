360e72df3842767364e4dc2f0fea56d40a3dd001
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-05-04 21:07:25 -0700

    Updated dataset

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,18 +1,4 @@
-#SCRIPT=`realpath $0`
-#SCRIPTPATH=`dirname $SCRIPT`
-#
-#cd $SCRIPTPATH/..
-#wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
-#unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
-#rm shapenetcore_partanno_segmentation_benchmark_v0.zip
-#cd -
-
-
-
-
-
-
 pip install gdown
-gdown --id 1Vr7thc3GAXNTpDuMfNmB-I1THKkdmwFL --output shapenetcore_partanno_segmentation_benchmark_v0.zip
+gdown --id 1b-ylF9DmfBvtTfhNJKeNq3Zs5W7yOHu5 --output shapenetcore_partanno_segmentation_benchmark_v0.zip
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
